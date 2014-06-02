96297c839435f6226e393e4ddc2ef51a607634ab
Max Rogers <maxrogers90@gmail.com>
2014-06-02 15:18:50 -0400

    added user signup

--- controller.rb
+++ controller.rb
@@ -3,11 +3,12 @@ get '/' do
   "Hello World"
 end
 
-put '/new_user' do 
-
+get '/new_user' do 
+	logger.info params.inspect
+	u = User.create(username: params[:email], password: params[:password])
+	jsonp "Made a New User#{u.id}"
 end
 
 get '/test_json' do
 	jsonp User.all
 end
-29942
