8776db63273c63e207b00731ae30a827240c2984
Max Rogers <maxrogers90@gmail.com>
2014-06-06 12:37:50 -0400

    adF

--- controller.rb
+++ controller.rb
@@ -6,7 +6,7 @@ end
 get '/new_user' do 
 	logger.info params.inspect
 	u = User.create(username: params[:email], password: params[:password])
-	jsonp "Made a New User#{u.id}"
+	jsonp u.to_json
 end
 
 get '/test_json' do
